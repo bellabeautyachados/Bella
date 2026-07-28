b65d3871de9f1d6114ca954ce190a3fe9f870f93
bellabeautyachados <achadinhorcp@gmail.com>
2026-07-27 23:34:58 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "achadinhos-ia-v2";
+const CACHE = "bella-v3";
 const FILES = ["./","./index.html","./style.css","./products.js","./app.js","./manifest.json","./icon.svg"];
 self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
 self.addEventListener("fetch", e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
