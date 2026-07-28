fe6bce6102824203b86c52ec668cbcd0a312fbfb
bellabeautyachados <achadinhorcp@gmail.com>
2026-07-28 01:49:04 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,63 +1,47 @@
-const CACHE = "bella-v15";
+const CACHE_NAME = "bella-v17";
 
-const FILES = [
+const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./style.css",
-  "./app.js",
   "./products.js",
+  "./app.js",
   "./manifest.json",
   "./logo.png"
 ];
 
 self.addEventListener("install", event => {
-  self.skipWaiting();
-
   event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(FILES))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
+
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys()
-      .then(keys =>
-        Promise.all(
-          keys
-            .filter(key => key !== CACHE)
-            .map(key => caches.delete(key))
-        )
-      )
-      .then(() => self.clients.claim())
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
   );
+
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  if (event.request.mode === "navigate") {
-    event.respondWith(
-      fetch(event.request)
-        .then(response => {
-          const copy = response.clone();
-
-          caches.open(CACHE).then(cache => {
-            cache.put("./index.html", copy);
-          });
-
-          return response;
-        })
-        .catch(() => caches.match("./index.html"))
-    );
-
-    return;
-  }
-
   event.respondWith(
     fetch(event.request)
       .then(response => {
-        const copy = response.clone();
+        const responseClone = response.clone();
 
-        caches.open(CACHE).then(cache => {
-          cache.put(event.request, copy);
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseClone);
         });
 
         return response;
